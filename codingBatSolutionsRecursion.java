8deb8391135b252176af35b012e8635c308df1a1
Brandon Bradley <brandonbradley@knights.ucf.edu>
2018-03-31 14:44:11 -0400

    Update codingBatSolutionsRecursion.java

--- codingBatSolutionsRecursion.java
+++ codingBatSolutionsRecursion.java
@@ -223,6 +223,33 @@ public class codingBatSolutionsRecursion {
                  return 1 + array11(nums, index+1);
           else return array11(nums, index+1);
       }
+  
+      /*
+      Given an array of ints, compute recursively if the array contains somewhere a value followed in the array by that 
+      value times 10. We'll use the convention of considering only the part of the array that begins at the given index. 
+      In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+      */
+      public boolean array220(int[] nums, int index) {
+
+          if (nums.length == 0)
+            return false;
+
+          if (nums.length == 1)
+            return false;
+
+          if (index == nums.length - 2) {
+
+            if (nums[index+1] == 10 * nums[index])
+                return true;
+
+            else return false;
+
+          }
+
+          if (nums[index+1] == 10 * nums[index])
+              return true;
+          return array220(nums, index+1);
+      }
 
 
 }
