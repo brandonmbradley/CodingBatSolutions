be2fbb5333efd55d86a5c09b71a70ec258740d3f
Brandon Bradley <brandonbradley@knights.ucf.edu>
2018-03-31 15:07:41 -0400

    Update codingBatSolutionsRecursion.java

--- codingBatSolutionsRecursion.java
+++ codingBatSolutionsRecursion.java
@@ -264,7 +264,25 @@ public class codingBatSolutionsRecursion {
 
           return str.charAt(0) + "*" + allStar(str.substring(1));
        }
+  
+       /*
+       Given a string, compute recursively a new string where identical chars that are adjacent in the original 
+       string are separated from each other by a "*".
+       */
+        public String pairStar(String str) {
 
-      
+            if (str.length() == 0)
+              return "";
+
+            if (str.length() == 1)
+              return str.charAt(0) + pairStar(str.substring(1));
+
+            if (str.charAt(0) == str.charAt(1))
+               return str.charAt(0) + "*" + pairStar(str.substring(1));
+
+            return str.charAt(0) + pairStar(str.substring(1));
+        }
+
+        
 
 }
